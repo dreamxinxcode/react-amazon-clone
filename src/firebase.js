0f871e246c93f269a2bdbaafa17c426f715a4729
dreamxinxcode <dreamxinxcode@gmail.com>
2020-10-15 01:21:43 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAB45mYV98hz7SgBSfx24DpD_RbYP-LM_s",
+  authDomain: "react--clone-68d45.firebaseapp.com",
+  databaseURL: "https://react--clone-68d45.firebaseio.com",
+  projectId: "react--clone-68d45",
+  storageBucket: "react--clone-68d45.appspot.com",
+  messagingSenderId: "280882935312",
+  appId: "1:280882935312:web:a37d312b6cf0cb45f33c95",
+};
