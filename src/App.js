2d54147a23a6268a1df6e4cefb12a50bd04ee59a
dreamxinxcode <dreamxinxcode@gmail.com>
2020-10-25 02:10:42 -0700

    fixed reducer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="app">
+        {/* Render the Header component in all routes */}
+        <Header />
+
         <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+
+          {/* Default route must be at the bottom */}
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './Product.scss';
 import StarRate from '@material-ui/icons/StarRate';
+import { useStateValue } from './StateProvider';
+
 
 function Product({id, title, image, price, rating}) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log('this is the basket:', basket);
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+  
   return (
     <div className='product'>
       <div className='product__info'>
@@ -19,7 +39,7 @@ function Product({id, title, image, price, rating}) {
       </div>
       <img src={image} alt="product_image"/>
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   )
 }
